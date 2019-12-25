b072bc4db5aabc39c7988394b465c3b14614e728
schaschjan <schasch.jan@gmail.com>
2019-12-25 12:35:05 +0100

    wip

--- src/views/admin/errors/index.js
+++ src/views/admin/errors/index.js
@@ -1,10 +1,44 @@
 import React, {useState, useEffect} from 'react';
+import ApiClient from "../../../ApiClient";
 
 export default function Index() {
 
+    const [data, setData] = useState(null);
+    const [loading, setLoading] = useState(true);
+
+    useEffect(() => {
+
+        ApiClient.getErrors().then(response => {
+            setData(Object.values(response));
+            setLoading(false);
+        });
+
+    }, []);
+
+    if (loading) {
+        return (
+            <div className="loader">
+                <em>loading</em>
+            </div>
+        )
+    }
+
+    console.log(data);
+
     return (
         <div className="container">
-            <h1>Errors</h1>
+            <h1>Errors ({data.length})</h1>
+
+            <ul className="list-unstyled">
+                {data.map(error => {
+                    return (
+                        <li>
+                            <strong>{error.author.username} @ {error.boulder.name} – {error.boulder.startWall.name}</strong>
+                            <p>{error.description}</p>
+                        </li>
+                    )
+                })}
+            </ul>
         </div>
     )
 }
