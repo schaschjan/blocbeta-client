55f93dd5c68ae66ec3d66b30445ea1299eb87e98
schaschjan <jan-jakub@atelierdisko.de>
2020-10-26 11:28:40 +0100

    tmp disable sentry

--- src/index.js
+++ src/index.js
@@ -7,13 +7,13 @@ import {Integrations} from "@sentry/tracing";
 import './index.css';
 
 if (process.env.NODE_ENV !== "development") {
-  Sentry.init({
-    dsn: "https://5c2038be7efd46a1b62ccba060e777a3@o456197.ingest.sentry.io/5448905",
-    integrations: [
-      new Integrations.BrowserTracing(),
-    ],
-    tracesSampleRate: 1.0,
-  });
+  // Sentry.init({
+  //   dsn: "https://5c2038be7efd46a1b62ccba060e777a3@o456197.ingest.sentry.io/5448905",
+  //   integrations: [
+  //     new Integrations.BrowserTracing(),
+  //   ],
+  //   tracesSampleRate: 1.0,
+  // });
 }
 
 export default Sentry.withProfiler(App);
