879a11736a66e20ae150e32ec1e8241a57f8b3b0
schaschjan <jan-jakub@atelierdisko.de>
2021-01-03 17:03:32 +0100

    integrate crip chat

--- src/index.js
+++ src/index.js
@@ -1,21 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import "./index.css";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
-import * as Sentry from "@sentry/react";
-import { Integrations } from "@sentry/tracing";
-import "./index.css";
-
-if (process.env.NODE_ENV !== "development") {
-  Sentry.init({
-    dsn:
-      "https://5c2038be7efd46a1b62ccba060e777a3@o456197.ingest.sentry.io/5448905",
-    integrations: [new Integrations.BrowserTracing()],
-    tracesSampleRate: 1.0,
-  });
-}
-
-export default Sentry.withProfiler(App);
 
 ReactDOM.render(<App />, document.getElementById("root"));
 
