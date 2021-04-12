af76e662eff32ad7191d8e54bf5c22a46d75ffdb
schaschjan <jan-jakub@atelierdisko.de>
2021-04-12 22:31:46 +0200

    reset on token expiry

--- src/hooks/useRequest.js
+++ src/hooks/useRequest.js
@@ -1,65 +1,73 @@
 import useSWR from "swr";
 import axios from "axios";
-import { useContext } from "react";
-import { BoulderDBUIContext } from "../components/BoulderDBUI";
+import {useContext} from "react";
+import {BoulderDBUIContext} from "../components/BoulderDBUI";
 
 let options = {
-  baseURL: process.env.REACT_APP_API_HOST,
-  headers: {},
+    baseURL: process.env.REACT_APP_API_HOST,
+    headers: {},
 };
 
 if (process.env.NODE_ENV !== "production" && typeof window !== "undefined") {
-  options.headers["Authorization"] = `Bearer ${localStorage.getItem("token")}`;
+    options.headers["Authorization"] = `Bearer ${localStorage.getItem("token")}`;
 }
 
 const axiosInstance = axios.create(options);
 
 function useUri(uri, locationResource = true) {
-  const { currentLocation } = useContext(BoulderDBUIContext);
+    const {currentLocation} = useContext(BoulderDBUIContext);
 
-  let url = "/api";
+    let url = "/api";
 
-  if (locationResource) {
-    if (!currentLocation) {
-      throw new Error("Unable to resolve current location");
+    if (locationResource) {
+        if (!currentLocation) {
+            throw new Error("Unable to resolve current location");
+        }
+
+        url += `/${currentLocation.url}`;
     }
 
-    url += `/${currentLocation.url}`;
-  }
+    url += uri;
 
-  url += uri;
-
-  return url;
+    return url;
 }
 
 const useApi = (uri, locationResource = true, requestConfig) => {
-  const url = useUri(uri, locationResource);
+    const url = useUri(uri, locationResource);
 
-  return async (data) =>
-    axiosInstance({
-      ...requestConfig,
-      url,
-      data,
-    });
+    return async (data) =>
+        axiosInstance({
+            ...requestConfig,
+            url,
+            data,
+        });
 };
 
 const useRequest = (
-  uri,
-  locationResource = true,
-  requestConfig = { method: "get" }
+    uri,
+    locationResource = true,
+    requestConfig = {method: "get"}
 ) => {
-  const url = useUri(uri, locationResource);
+    const {reset} = useContext(BoulderDBUIContext);
+    const url = useUri(uri, locationResource);
 
-  return useSWR(url, async (url) => {
-    const { data } = await axiosInstance({
-      ...requestConfig,
-      url,
+    return useSWR(url, async (url) => {
+
+        try {
+            const {data} = await axiosInstance({
+                ...requestConfig,
+                url,
+            });
+
+            return data;
+        } catch (error) {
+            if (error.response.status === 401) {
+                reset();
+            }
+        }
     });
-
-    return data;
-  });
 };
 
 const useHttp = () => axiosInstance;
 
-export { useRequest, useApi, useUri, useHttp };
+export {useRequest, useApi, useUri, useHttp};
